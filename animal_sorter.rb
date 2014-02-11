d1b98aed6a054ce520198ec1177429fd044b3d88
Logan Hasson <logan.hasson@gmail.com>
2014-02-11 10:00:11 -0500

    Complete solution

--- animal_sorter.rb
+++ animal_sorter.rb
@@ -7,3 +7,23 @@
 # should include the sea creatures, the second, land animals.
 
 # Read the test suite for an example of a nested array.
+
+class AnimalSorter
+  attr_reader :animals
+
+  SEA_ANIMALS = ["marlin", "octopus", "fish"]
+  LAND_ANIMALS = ["aardvark", "cat", "elephant"]
+
+  def initialize(animals)
+    @animals = animals
+  end
+
+  def to_a
+    self.animals.inject([[],[]]) do |memo, animal|
+      memo[0] << animal if SEA_ANIMALS.include?(animal)
+      memo[1] << animal if LAND_ANIMALS.include?(animal)
+      memo
+    end
+  end
+
+end
